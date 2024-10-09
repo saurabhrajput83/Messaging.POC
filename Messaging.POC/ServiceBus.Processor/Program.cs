9a2fd078edad419d520cfd0e1f862b17c3d36134
Saurabh <saurabh.rajput@hotmail.com>
2024-10-09 18:07:08 +0530

    Added ServiceBus.Processor.

---
+++ Messaging.POC/ServiceBus.Processor/Program.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Messaging.ServiceBus;
+using ServiceBus.BLL.Implementations;
+using System;
+using System.Threading.Tasks;
+
+
+string namespace_connection_string = "";
+string queue_name = "";
+
+ServiceBusQueueManager manager = new ServiceBusQueueManager(namespace_connection_string, queue_name);
+
+await manager.StartListening(MessageHandler, ErrorHandler);
+
+
+
+
+
+
+// handle received messages
+async Task MessageHandler(ProcessMessageEventArgs args)
+{
+    string body = args.Message.Body.ToString();
+    Console.WriteLine($"Received: {body}");
+
+    // complete the message. message is deleted from the queue. 
+    await args.CompleteMessageAsync(args.Message);
+}
+
+// handle any errors when receiving messages
+Task ErrorHandler(ProcessErrorEventArgs args)
+{
+    Console.WriteLine(args.Exception.ToString());
+    return Task.CompletedTask;
+}
