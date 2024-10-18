a12b9df88fd259e620428247a655e2fa75465640
Saurabh <saurabh.rajput@hotmail.com>
2024-10-18 16:32:48 +0530

    Add/Updated Messaging.POC.Publisher.

---
+++ Messaging.POC/Messaging.POC.Publisher/Program.cs
@@ -0,0 +1,44 @@
+﻿using Messaging.POC.BLL;
+using Messaging.POC.BLL.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Messaging.POC.Publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            IManager manager = new Manager();
+
+            while (flag)
+            {
+                Console.WriteLine("Press a to test TIBCO RV, b to test Service Bus, and x to exit..");
+                var line = Console.ReadLine().ToUpper();
+
+
+                switch (line)
+                {
+
+                    case "A":
+                        manager.GetPublisher(MessagingType.TIBCO_RV).Run();
+                        break;
+                    case "B":
+                        manager.GetPublisher(MessagingType.Service_Bus).Run();
+                        break;
+                    case "X":
+                        flag = false;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+    }
+}
