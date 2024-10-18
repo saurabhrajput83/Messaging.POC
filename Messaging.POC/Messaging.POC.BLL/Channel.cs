fdd3b70559ac4d3ea3803a598e41e50ccd9bd76f
Saurabh <saurabh.rajput@hotmail.com>
2024-10-18 12:44:35 +0530

    Updated Messaging.POC.BLL.

---
+++ Messaging.POC/Messaging.POC.BLL/Channel.cs
@@ -0,0 +1,57 @@
+﻿using Messaging.POC.BLL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TIBCO.Rendezvous;
+
+namespace Messaging.POC.BLL
+{
+    public class Channel
+    {
+        private Transport _transport;
+
+        public Channel(Transport transport)
+        {
+            _transport = transport;
+        }
+
+        public void SendMessage(CustomMessage customMsg)
+        {
+            Message msg = ConvertToTibcoMessage(customMsg);
+
+            _transport.Send(msg);
+        }
+
+        private Message ConvertToTibcoMessage(CustomMessage customMsg)
+        {
+            Message msg = new Message();
+
+            msg.SendSubject = customMsg.SendSubject;
+            msg.ReplySubject = customMsg.ReplySubject;
+
+            msg.AddField(new MessageField("Name", customMsg.Name));
+            msg.AddField(new MessageField("Age", customMsg.Age));
+            msg.AddField(new MessageField("Department", customMsg.Department));
+            msg.AddField(new MessageField("Address", customMsg.Address));
+
+            return msg;
+        }
+
+        private CustomMessage ConvertToCustomMessage(Message msg)
+        {
+            CustomMessage customMsg = new CustomMessage();
+
+            customMsg.SendSubject = msg.SendSubject;
+            customMsg.ReplySubject = msg.ReplySubject;
+
+            customMsg.Name = (string)msg.GetField("Name").Value;
+            customMsg.Age = (int)msg.GetField("Age").Value;
+            customMsg.Department = (string)msg.GetField("Department").Value;
+            customMsg.Address = (string)msg.GetField("Address").Value;
+
+            return customMsg;
+        }
+    }
+}
---
+++ Messaging.POC/Messaging.POC.BLL/DTOs/CustomMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messaging.POC.BLL.DTOs
+{
+    public class CustomMessage
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Department { get; set; }
+        public string Address { get; set; }
+        public string SendSubject { get; set; }
+        public string ReplySubject { get; set; }
+    }
+}
