ea0361da53ddd5f64f80532cf7f0a5315fc62213
Saurabh <saurabh.rajput@hotmail.com>
2024-10-18 12:14:08 +0530

    Added TIBCO.RV.Publisher.

---
+++ Messaging.POC/TIBCO.RV.Publisher/Publisher.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TIBCO.Rendezvous;
+
+namespace TIBCO.RV.Publisher
+{
+    public class Publisher
+    {
+        public void Run()
+        {
+            try
+            {
+                TIBCO.Rendezvous.Environment.Open();
+                var subject = "ME.TEST";
+                string service = ConfigurationManager.AppSettings["service"];
+                string network = ConfigurationManager.AppSettings["network"];
+                string daemon = ConfigurationManager.AppSettings["daemon"];
+
+
+                var transport = new NetTransport(service, network, daemon);
+                Console.WriteLine("Server running..");
+                Console.WriteLine("Press x to exit or any other key to send message");
+                
+                while (true)
+                {
+                    var m = new Message();
+                    m.SendSubject = subject;
+                    m.AddField("Test", "TestValue");
+                    transport.Send(m);
+                    var line = Console.ReadLine();
+                    if (line.ToUpper().Equals("X"))
+                        break;
+                }
+
+                TIBCO.Rendezvous.Environment.Close();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
