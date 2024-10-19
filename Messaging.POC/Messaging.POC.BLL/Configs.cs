d1a0b5126a1fb4b2a53b044a5731e89c532eae70
Saurabh <saurabh.rajput@hotmail.com>
2024-10-19 17:08:39 +0530

    Added Configs.

---
+++ Messaging.POC/Messaging.POC.BLL/Configs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messaging.POC.BLL
+{
+    public class Configs
+    {
+        public static string SERVICE = ConfigurationManager.AppSettings["service"];
+        public static string NETWORK = ConfigurationManager.AppSettings["network"];
+        public static string DAEMON = ConfigurationManager.AppSettings["daemon"];
+
+        public static string NAMESPACE_CONNECTION_STRING = ConfigurationManager.AppSettings["namespace_connection_string"];
+        public static string TOPIC_OR_QUEUE_NAME = ConfigurationManager.AppSettings["topic_or_queue_name"];
+        public static string SUBSCRIPTION_NAME = ConfigurationManager.AppSettings["subscription_name"];
+
+        public static string SENDMESSAGESUBJECT = ConfigurationManager.AppSettings["sendMessageSubject"];
+        public static string SENDREQUESTMESSAGESUBJECT = ConfigurationManager.AppSettings["sendRequestMessageSubject"];
+        public static string SENDREPLYMESSAGESUBJECT = ConfigurationManager.AppSettings["sendReplyMessageSubject"];
+
+    }
+}
