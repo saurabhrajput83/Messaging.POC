c7d209a5bcc0edd52f7f513c186ba6d1676dafd1
Saurabh <saurabh.rajput@hotmail.com>
2024-10-25 08:39:23 +0530

    Added TIBCORV.Framework.

---
+++ Messaging.POC/TIBCORV.Framework/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TIBCO.Rendezvous;
+
+namespace TIBCORV.Framework
+{
+    public class Class1
+    {
+        public void Test()
+        {
+            TIBCO.Rendezvous.Message msg = new TIBCO.Rendezvous.Message();
+
+            //Trans
+
+            //Transport
+
+            //msg.GetField()
+        }
+    }
+}
---
+++ Messaging.POC/TIBCORV.Framework/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TIBCORV.Framework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TIBCORV.Framework")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("308c93f4-e203-4454-9b23-efb2eb25c60a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
